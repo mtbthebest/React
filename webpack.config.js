17679c914d3b92294f1b3ed9acd07155f07421d8
telli_barry <mtbthebest11@gmail.com>
2018-11-18 22:03:33 +0900

    change app directory

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,11 @@ var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
     mode: "development",
-    entry: SRC_DIR + "/app/index.js",
+    entry: SRC_DIR + "/jsx/app/index.js",
     output: {
-        path: DIST_DIR + "/app",
+        path: DIST_DIR + "/jsx/app",
         filename: "bundle.js",
-        publicPath: "/app/"
+        publicPath: "/jsx/app/"
     },
     module: {
         rules:[
