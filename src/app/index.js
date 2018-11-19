fbc5be24e2b63489539d762406a5267e48f73826
telli_barry <mtbthebest11@gmail.com>
2018-11-19 22:25:28 +0900

    create rating start

--- src/app/index.js
+++ src/app/index.js
@@ -3,34 +3,7 @@ import createClass  from 'create-react-class'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 
-// class  AddClassForm extends React.Component{
-//     constructor(props){
-//         super(props)
-//         this.submit = this.submit.bind(this)
-//     }
-//     submit(e){
-//         const { _title, _color } = this.refs
-//         e.preventDefault()
-//         alert(`New color: ${_title.value} ${ _color.value }`)
-//         _title.value =""
-//         _color.value ='#000000'
-//         _title.focus()
-//     }
-//
-//     render(){
-//         return( <form onSubmit={this.submit}>
-//             <input ref="_title" type="text" placeholder="color title ..." required />
-//             <input ref="_color" type="color" required/>
-//             <button>ADD</button>
-//         </form>)
-//     }
-//
-// }
-//
-// ReactDOM.render(
-//     <AddClassForm/>,
-//     document.getElementById("react-container")
-// )
+
 
 
 const Star = ({ selected=false, onClick=f=>f })=>
@@ -42,29 +15,24 @@ Star.propTypes={
     onClick: PropTypes.func
 }
 
-const StartRating  = createClass({
-    displayName: 'StarRating',
-    propTypes:{
-        totalStars: PropTypes.number
-    },
-    getDefaultProps(){
-        return{
-            totalStars :5
+class StartRating  extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+            starsSelected : props.starsSelected || 0
         }
-    },
-    getInitialState(){
-        return {
-            starsSelected : 0
-        }
-    },
+        this.change = this.change.bind(this)
+    }
+
     change(starsSelected){
         this.setState({starsSelected})
-    },
+    }
     render(){
         const {totalStars} = this.props
         const  {starsSelected} = this.state
         return (
-            <div className='star-raing'>
+            <div className='star-rating'>
                 {[...Array(totalStars)].map((n,i) =>
                     <Star key ={i} selected={i<starsSelected}
                         onClick = {() => this.change(i+1)}
@@ -74,9 +42,16 @@ const StartRating  = createClass({
             </div>
         )
     }
-})
+}
+
+StartRating.propTypes = {
+    totalStars: PropTypes.number
+}
+StartRating.defaultProps = {
+    totalStars: 5
+}
 
 ReactDOM.render(
-    <StartRating/>,
+    <StartRating totalStars={7} starsSelected={3}/>,
     document.getElementById("react-container")
 )
