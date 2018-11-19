ce326def42699d7b7c5135a977c24ecc5b3c7116
telli_barry <mtbthebest11@gmail.com>
2018-11-19 20:47:31 +0900

    color with input

---
+++ src/app/index.js
@@ -0,0 +1,32 @@
+import React   from 'react';
+import ReactDOM from 'react-dom'
+
+class  AddClassForm extends React.Component{
+    constructor(props){
+        super(props)
+        this.submit = this.submit.bind(this)
+    }
+    submit(e){
+        const { _title, _color } = this.refs
+        e.preventDefault()
+        alert(`New color: ${_title.value} ${ _color.value }`)
+        _title.value =""
+        _color.value ='#000000'
+        _title.focus()
+    }
+
+    render(){
+        return( <form onSubmit={this.submit}>
+            <input ref="_title" type="text" placeholder="color title ..." required />
+            <input ref="_color" type="color" required/>
+            <button>ADD</button>
+        </form>)
+    }
+
+}
+
+ReactDOM.render(
+    <AddClassForm/>,
+    document.getElementById("react-container")
+)
+
